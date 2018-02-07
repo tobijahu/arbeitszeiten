1fa94faac278ff4cf73c7f410fcc4322229624d6
Tobias Mettenbrink <tobijahu@users.noreply.github.com>
2018-02-07 06:35:28 +0100

    adjusted tests to rewritten functions

--- test/test_libarbeitszeiten.py
+++ test/test_libarbeitszeiten.py
@@ -23,22 +23,23 @@ class TestStringMethods(unittest.TestCase):
                            *) Ausdrücke mit Zeichen nicht aus '0-9:-' (negativ)
                            *) nicht-String-Ausdrücke (negativ)
         '''
-        # Äquivalenzklassen / Grenzwertanalyse
+        # Grenzwertanalyse
         self.assertFalse(liba.ist_zeitstring('-1:01'))
         self.assertFalse(liba.ist_zeitstring('-0:01'))
         self.assertFalse(liba.ist_zeitstring('-1:0'))
         self.assertTrue(liba.ist_zeitstring('0:0'))
         self.assertTrue(liba.ist_zeitstring('23:59'))
         self.assertTrue(liba.ist_zeitstring('24:00'))
-        self.assertTrue(liba.ist_zeitstring('23:60'))
+        self.assertFalse(liba.ist_zeitstring('23:60'))
         self.assertFalse(liba.ist_zeitstring('24:01'))
         self.assertFalse(liba.ist_zeitstring('23:61'))
         self.assertFalse(liba.ist_zeitstring('25:00'))
-        # Interessante Werte
+        # Äquivalenzklassen
         self.assertTrue(liba.ist_zeitstring('09:55'))
         self.assertTrue(liba.ist_zeitstring('9:5'))
         self.assertTrue(liba.ist_zeitstring('19:5'))
         self.assertFalse(liba.ist_zeitstring('99:99'))
+        self.assertFalse(liba.ist_zeitstring('-09:05'))
         self.assertFalse(liba.ist_zeitstring('-:1'))
         # Ausdrücke ohne ':'
         self.assertFalse(liba.ist_zeitstring('24'))
@@ -58,18 +59,18 @@ class TestStringMethods(unittest.TestCase):
         Weitere:           *) Interessante Werte (positiv)
                            *) Nicht-erlaubte Zeichen (negativ)
         '''
+        # Äquivalenzklassen / Grenzwertanalyse
         self.assertFalse(liba.ist_minuten(-1))
         self.assertTrue(liba.ist_minuten(0))
         self.assertTrue(liba.ist_minuten(1))
-        self.assertFalse(liba.ist_minuten('-1'))
-        self.assertTrue(liba.ist_minuten('0'))
-        self.assertTrue(liba.ist_minuten('1'))
-        # Interessante Werte:
         self.assertTrue(liba.ist_minuten(59))
         self.assertTrue(liba.ist_minuten(60))
         self.assertTrue(liba.ist_minuten(61))
         self.assertTrue(liba.ist_minuten(24*60))
         self.assertTrue(liba.ist_minuten(24*60+1))
+        self.assertFalse(liba.ist_minuten('-1'))
+        self.assertTrue(liba.ist_minuten('0'))
+        self.assertTrue(liba.ist_minuten('1'))
         self.assertTrue(liba.ist_minuten('60'))
         self.assertTrue(liba.ist_minuten('61'))
         self.assertTrue(liba.ist_minuten('1440'))
@@ -87,6 +88,11 @@ class TestStringMethods(unittest.TestCase):
         Weitere            *) Tripel (negativ)
                            *) Nicht-erlaubte Zeichen (negativ)
         '''
+        self.assertEqual(liba.zeitstring_zu_minuten("00:00"), 0)
+        self.assertEqual(liba.zeitstring_zu_minuten("00:01"), 1)
+        self.assertEqual(liba.zeitstring_zu_minuten("01:00"), 1*60)
+        self.assertEqual(liba.zeitstring_zu_minuten("23:59"), 23*60+59)
+        self.assertEqual(liba.zeitstring_zu_minuten("24:00"), 24*60)
         with self.assertRaises(ValueError):
             liba.zeitstring_zu_minuten("-01:00")
         with self.assertRaises(ValueError):
@@ -95,12 +101,8 @@ class TestStringMethods(unittest.TestCase):
             liba.zeitstring_zu_minuten("00:-01")
         with self.assertRaises(ValueError):
             liba.zeitstring_zu_minuten("00:-1")
-        self.assertEqual(liba.zeitstring_zu_minuten("00:00"), 0)
-        self.assertEqual(liba.zeitstring_zu_minuten("00:01"), 1)
-        self.assertEqual(liba.zeitstring_zu_minuten("01:00"), 1*60)
-        self.assertEqual(liba.zeitstring_zu_minuten("23:59"), 23*60+59)
-        self.assertEqual(liba.zeitstring_zu_minuten("24:00"), 24*60)
-        self.assertEqual(liba.zeitstring_zu_minuten("23:60"), 23*60+60)
+        with self.assertRaises(ValueError):
+            liba.zeitstring_zu_minuten("23:60")
         with self.assertRaises(ValueError):
             liba.zeitstring_zu_minuten("24:01")
         with self.assertRaises(ValueError):
@@ -124,12 +126,12 @@ class TestStringMethods(unittest.TestCase):
 
     def test_filter_zpkte_pausen(self):
         self.assertEqual(liba.filter_zpkte_pausen( \
-                         [(8, 15), 55, (12, 15), (13, 0), 30]), \
+                         ["8:15", 55, "12:15", "13:0", 30]), \
                          ([8*60+15, 12*60+15, 13*60], [55, 30]))
-        with self.assertRaises(TypeError):
-            liba.filter_zpkte_pausen([(8, 15), 55, (12, 15), (25, 0), (13, 0), 30])
-        with self.assertRaises(TypeError):
-            liba.filter_zpkte_pausen([(8, 15), 55, (12, 15), (22, 66), (13, 0), 30])
+        with self.assertRaises(ValueError):
+            liba.filter_zpkte_pausen(["8:15", 55, "12:15", "25:0", "13:0", 30])
+        with self.assertRaises(ValueError):
+            liba.filter_zpkte_pausen(["8:15", 55, "12:15", "22:66", "13:0", 30])
 
     def test_intervall_summe(self):
         # 12:00 - 08:00 + 16:30 - 12:30 - 30min
@@ -151,53 +153,53 @@ class TestStringMethods(unittest.TestCase):
             liba.intervall_summe("['08:00', 30, '16:30']")
 
     def test_auswerten(self):
-        self.assertEqual(liba.auswerten([(8, 0), (12, 0), (12, 30), (16, 30)], 8*60), \
+        self.assertEqual(liba.auswerten(["8:0", "12:0", "12:30", "16:30"], 8*60), \
                          (480, None, None, 30, 0, True))
         # 2 Zeitpunkte und Pausendauer:
-        self.assertEqual(liba.auswerten([(8, 0), 30, (16, 30)], 8*60), \
+        self.assertEqual(liba.auswerten(["8:0", 30, "16:30"], 8*60), \
                          (480, None, None, 30, 0, True))
         # 2 Zeitpunkte und 2 Pausendauern:
-        self.assertEqual(liba.auswerten([45, (8, 0), 30, (16, 30)], 8*60), \
+        self.assertEqual(liba.auswerten([45, "8:0", 30, "16:30"], 8*60), \
                          (435, None, None, 75, 0, True))
-        self.assertEqual(liba.auswerten([(8, 0), 30, (16, 30), 45], 8*60), \
+        self.assertEqual(liba.auswerten(["8:0", 30, "16:30", 45], 8*60), \
                          (435, None, None, 75, 0, True))
         # 1 Zeitpunkt und 2 Pausendauern:
-        self.assertEqual(liba.auswerten([45, 30, (16, 30)], 8*60), \
-                         (None, 7*60+15, None, 75, 0, True))
-        self.assertEqual(liba.auswerten([45, 30, (16, 30)], 8*60, start_gegeben=False), \
-                         (None, 7*60+15, None, 75, 0, True))
-        self.assertEqual(liba.auswerten([(8, 0), 30, 46], 8*60), \
-                         (None, None, 17*60+16, 30+46, 0, True))
+        self.assertEqual(liba.auswerten([45, 30, "16:30"], 8*60), \
+                         (8*60, 7*60+15, None, 75, 0, True))
+        self.assertEqual(liba.auswerten([45, 30, "16:30"], 8*60, start_gegeben=False), \
+                         (8*60, 7*60+15, None, 75, 0, True))
+        self.assertEqual(liba.auswerten(["8:0", 30, 46], 8*60), \
+                         (8*60, None, 17*60+16, 30+46, 0, True))
         # 3 Zeitpunkte:
-        self.assertEqual(liba.auswerten([(12, 0), (12, 30), (16, 30)], \
+        self.assertEqual(liba.auswerten(["12:0", "12:30", "16:30"], \
                                    8*60, start_gegeben=False), \
-                         (None, 8*60, None, 30, 0, True))
-        self.assertEqual(liba.auswerten([(12, 0), (12, 30), (16, 30)], 8*60), \
-                         (None, None, 24*60, 4*60, 0, True))
-        self.assertEqual(liba.auswerten([(8, 0), (12, 00), (12, 30)], 8*60), \
-                         (None, None, (8+8)*60+30, 30, 0, True))
+                         (8*60, 8*60, None, 30, 0, True))
+        self.assertEqual(liba.auswerten(["12:0", "12:30", "16:30"], 8*60), \
+                         (8*60, None, 24*60, 4*60, 0, True))
+        self.assertEqual(liba.auswerten(["8:0", "12:00", "12:30"], 8*60), \
+                         (8*60, None, (8+8)*60+30, 30, 0, True))
         # unsortierte Zeitpunkte
-        self.assertEqual(liba.auswerten([(16, 30), (12, 0)], 8*60), \
+        self.assertEqual(liba.auswerten(["16:30", "12:0"], 8*60), \
                          (16*60+30 - 12*60, None, None, 0, 0, True))
-        self.assertEqual(liba.auswerten([(16, 30), (12, 0), (12, 30)], 8*60), \
-                         (None, None, 24*60, 16*60+30-(12*60+30), 0, True))
-        self.assertEqual(liba.auswerten([(13, 0), (7, 0), (12, 30)], 8*60), \
-                         (None, None, 15*60+30, 30, 0, True))
-        self.assertEqual(liba.auswerten([(13, 0), (16, 30), (12, 30)], 8*60, False), \
-                         (None, 8*60, None, 30, 0, True))
+        self.assertEqual(liba.auswerten(["16:30", "12:0", "12:30"], 8*60), \
+                         (8*60, None, 24*60, 16*60+30-(12*60+30), 0, True))
+        self.assertEqual(liba.auswerten(["13:0", "7:0", "12:30"], 8*60), \
+                         (8*60, None, 15*60+30, 30, 0, True))
+        self.assertEqual(liba.auswerten(["13:0", "16:30", "12:30"], 8*60, False), \
+                         (8*60, 8*60, None, 30, 0, True))
         # nicht-konforme Pausenzeiten
-        self.assertEqual(liba.auswerten([5, (16, 30)], 8*60), \
-                         (None, 8*60+25, None, 5, 25, False))
+        self.assertEqual(liba.auswerten([5, "16:30"], 8*60), \
+                         (8*60, 8*60+25, None, 5, 25, False))
         with self.assertRaises(AssertionError):
-            liba.auswerten("[(12,0), (12,30), (16,30)]", 50)
+            liba.auswerten('["12:0", "12:30", "16:30"]', 50)
         with self.assertRaises(AssertionError):
-            liba.auswerten([(12, 0), (12, 30), (16, 30)], '50')
+            liba.auswerten(["12:0", "12:30", "16:30"], '50')
         with self.assertRaises(AssertionError):
-            liba.auswerten([(12, 0), (12, 30), (16, 30)], 50, 0)
-        with self.assertRaises(TypeError):
-            liba.auswerten([(12, 0), (12, 30), (24, 1)], 50)
+            liba.auswerten(["12:0", "12:30", "16:30"], 50, 0)
         with self.assertRaises(ValueError):
-            liba.auswerten([(12, 0), (12, 30), (16, 30)], -50)
+            liba.auswerten(["12:0", "12:30", "24:1"], 50)
+        with self.assertRaises(ValueError):
+            liba.auswerten(["12:0", "12:30", "16:30"], -50)
 
     def test_pausengesetz_vereinfacht(self):
         self.assertTrue(liba.pausengesetz_vereinfacht(6*60, 0))
